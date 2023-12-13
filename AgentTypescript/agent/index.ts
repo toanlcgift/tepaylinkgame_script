ac052163ef3633d0165289d10001c68eef31bd63
toanlcgift <toanlcgift@gmail.com>
2023-12-14 00:13:11 +0700

    try another hook, but still cannot find target native lib address. Logcat shows some address but it's not good solution.

--- AgentTypescript/agent/index.ts
+++ AgentTypescript/agent/index.ts
@@ -1,18 +1,35 @@
+Java.perform(function () {
+    const System = Java.use('java.lang.System');
+    const Runtime = Java.use('java.lang.Runtime');
+    const SystemLoad_2 = System.loadLibrary.overload('java.lang.String');
+    const VMStack = Java.use('dalvik.system.VMStack');
+
+    SystemLoad_2.implementation = function (library: any) {
+        send("Loading dynamic library => " + library);
+        try {
+            const loaded = Runtime.getRuntime().loadLibrary0(VMStack.getCallingClassLoader(), library);
+            if (library === 'MyGame') {
+                console.log("found lib, but cannot get module address");
+            }
+            return loaded;
+        } catch (ex) {
+            console.log(ex);
+        }
+    };
+});
+
 var didHookApis = false;
-Interceptor.attach(Module.findExportByName(null, 'dlopen') ?? new NativePointer(0x00), {
+Interceptor.attach(Module.getExportByName('libnb.so', 'NativeBridgeItf') ?? new NativePointer(0x00), {
     onEnter: function (args) {
+        console.log("libnb");
         console.log(args[0].readUtf8String());
         this.path = args[0].readUtf8String();
     },
     onLeave: function (retval) {
-        if (!retval.isNull() && this.path.indexOf('libMyGame.so') !== -1 && !didHookApis) {
-            didHookApis = true;
-            console.log("File loaded hooking");
-            hooknative2();
-            // ...
-        }
     }
 });
+
+Process.enumerateModules().filter(x => x.name == 'libnb.so')[0].enumerateExports()
 
 function hooknative2() {
     console.log("hooknative2");
