0e27100e69ddf4a215771f5188f41e784b532fb7
toanlcgift <toanlcgift@gmail.com>
2024-01-07 12:51:16 +0700

    update

--- AgentTypescript/agent/cclognativecall.ts
+++ AgentTypescript/agent/cclognativecall.ts
@@ -6,3 +6,11 @@ logFunctionCall(value);
 var getVersionFunctionAddr = Module.findExportByName('libMyGame.so', '_ZN7cocos2d14cocos2dVersionEv') ?? new NativePointer(0x00);
 var getVersionFunctionCall = new NativeFunction(getVersionFunctionAddr, 'pointer', []);
 console.log(getVersionFunctionCall().readUtf8String());
+
+var directorGetInstance = Module.findExportByName('libMyGame.so', '_ZN7cocos2d8Director11getInstanceEv') ?? new NativePointer(0x00);
+var directorGetInstanceCall = new NativeFunction(directorGetInstance, 'pointer', []);
+var instancePtr = directorGetInstanceCall();
+
+//var pauseFunc = Module.findExportByName('libMyGame.so', '_ZN7cocos2d8Director8popSceneEv') ?? new NativePointer(0x00);
+//var pauseFuncCall = new NativeFunction(pauseFunc, 'void', ['pointer']);
+//pauseFuncCall(instancePtr);
--- AgentTypescript/agent/index.ts
+++ AgentTypescript/agent/index.ts
@@ -1,36 +1,69 @@
-Java.perform(function () {
-    const System = Java.use('java.lang.System');
-    const Runtime = Java.use('java.lang.Runtime');
-    const SystemLoad_2 = System.loadLibrary.overload('java.lang.String');
-    const VMStack = Java.use('dalvik.system.VMStack');
+const BUF_SIZE = 16;
 
-    SystemLoad_2.implementation = function (library: any) {
-        send("Loading dynamic library => " + library);
-        try {
-            const loaded = Runtime.getRuntime().loadLibrary0(VMStack.getCallingClassLoader(), library);
-            if (library === 'MyGame') {
-                console.log("found lib, but cannot get module address");
-            }
-            return loaded;
-        } catch (ex) {
-            console.log(ex);
+class StdString {
+    addr: NativePointer;
+    constructor(addr: NativePointer) {
+        this.addr = addr;
+    }
+
+    get bufAddr() {
+        if (this.reservedSize.compare(16) > 0) {
+            return this.addr.readPointer();
+        } else {
+            return this.addr;
         }
-    };
-});
+    }
 
-var didHookApis = false;
-Interceptor.attach(Module.getExportByName('libnb.so', 'NativeBridgeItf') ?? new NativePointer(0x00), {
+    get size() {
+        return this.addr.add(BUF_SIZE).readPointer();
+    }
+
+    get reservedSize() {
+        return this.addr.add(BUF_SIZE).add(Process.pointerSize).readPointer();
+    }
+
+    toString() {
+        const size = this.size;
+        if (size.isNull()) {
+            return "<EMPTY std::string>";
+        }
+        return this.bufAddr.readCString(size.toInt32());
+    }
+}
+
+
+var i = 0;
+
+ function getStdString(input: NativePointer): string | null {
+    var isTiny = (input.readU8() & 1) === 0;
+    if (isTiny) {
+        return input.add(1).readUtf8String();
+    }
+    return input.add(2 * Process.pointerSize).readPointer().readUtf8String();
+}
+
+Interceptor.attach(Module.getExportByName('libMyGame.so', '_ZN7cocos2d6GLView11renderSceneEPNS_5SceneEPNS_8RendererE') ?? new NativePointer(0x00), {
     onEnter: function (args) {
-        console.log("libnb");
-        console.log(args[0].readUtf8String());
-        this.path = args[0].readUtf8String();
+        if (i++ == 0) {
+            var currentScene = args[0].readPointer();
+            console.log(currentScene);
+            var createSpriteFunc = Module.findExportByName('libMyGame.so', '_ZN7cocos2d6Sprite6createERKNSt6__ndk112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEE') ?? new NativePointer(0x00);
+            var createSpriteFunctionCall = new NativeFunction(createSpriteFunc, 'pointer', ['pointer']);
+            var sprite = createSpriteFunctionCall((new StdString(Memory.allocUtf8String("bgloading.jpg")).bufAddr));
+            console.log(sprite);
+            var addChildFunc = Module.findExportByName('libMyGame.so', '_ZN7cocos2d4Node8addChildEPS0_i') ?? new NativePointer(0x00);
+            var addChildFuncCall = new NativeFunction(addChildFunc, 'uint', ['pointer', 'pointer', 'int']);
+            addChildFuncCall(currentScene, sprite, 100);
+        }
     },
     onLeave: function (retval) {
     }
 });
 
-Process.enumerateModules().filter(x => x.name == 'libnb.so')[0].enumerateExports()
-
-function hooknative2() {
-    console.log("hooknative2");
-}
+Interceptor.attach(Module.getExportByName('libMyGame.so', '_ZN7cocos2d6Sprite6createERKNSt6__ndk112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEE') ?? new NativePointer(0x00), {
+    onEnter: function (args) {
+        console.log(getStdString(args[0]));
+    },
+    onLeave: function (retval) {
+    }
+});
